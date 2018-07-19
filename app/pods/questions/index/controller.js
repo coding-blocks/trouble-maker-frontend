de2deba617da2a2b19f68e34f56c94df58f52239
fire-finch <firefinchdev@gmail.com>
2018-07-20 05:19:31 +0530

    SearchBox for Questions

--- app/pods/questions/index/controller.js
+++ app/pods/questions/index/controller.js
@@ -1,8 +1,27 @@
 import Controller from '@ember/controller';
+import { task, timeout } from 'ember-concurrency';
 
 export default Controller.extend({
   queryParams: ['page', 'limit'],
   page: 1,
-  limit: 10
+  limit: 10,
+  searchString: '',
+  searchTask: task(function * () {
+    yield timeout(250)
+    const questions = yield this.get('store').query('question', {
+      include: 'user',
+      page: {
+        number: this.get('page'),
+        limit: this.get('limit')
+      },
+      filter: {
+        title: {
+          $iLike: `%${this.get('searchString')}%`
+        }
+      }
+    })
+    this.set('page', 1)
+    this.set('questions', questions)
+  }).restartable()
 
 });
