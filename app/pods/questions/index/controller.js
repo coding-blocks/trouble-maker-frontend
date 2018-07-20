9f8aeff33a2787a9d860c0eb0ccd1ef73be6bb65
fire-finch <firefinchdev@gmail.com>
2018-07-20 16:00:18 +0530

    Fix: Removed limit On Filteration

--- app/pods/questions/index/controller.js
+++ app/pods/questions/index/controller.js
@@ -8,12 +8,24 @@ export default Controller.extend({
   searchString: '',
   searchTask: task(function * () {
     yield timeout(250)
+
+    let searchStr = this.get('searchString').trim()
+    let pageOpts = {}
+    this.set('searchString', searchStr)
+
+    if(searchStr === '') {
+      pageOpts = {
+        number: 1,
+        limit: this.get('limit')
+      }
+    } else {
+      pageOpts = {
+        number: 1
+      }
+    }
     const questions = yield this.get('store').query('question', {
       include: 'user',
-      page: {
-        number: this.get('page'),
-        limit: this.get('limit')
-      },
+      page: pageOpts,
       filter: {
         title: {
           $iLike: `%${this.get('searchString')}%`
