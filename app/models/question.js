a05d023d147ae760f5dccd7dbac1c5086425b8a2
Jatin Katyal <jatin.katyal13@gmail.com>
2020-04-22 19:28:12 +0530

    score is now part of the question

--- app/models/question.js
+++ app/models/question.js
@@ -4,6 +4,8 @@ export default DS.Model.extend({
   title: DS.attr(),
   description: DS.attr(),
   difficulty: DS.attr(),
+  positiveScore: DS.attr(),
+  negativeScore: DS.attr(),
   user: DS.belongsTo('user'),
   choices: DS.hasMany('choice'),
   tags: DS.hasMany('tag')
