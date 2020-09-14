0cb4413b95e3018295722bcb8deaa3de4e8f711b
abhishek97 <hi@itsabhishek.me>
2020-09-14 19:43:12 +0530

    package maintainence; use node 10

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,13 +3,23 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import env from 'trouble-maker-frontend/config/environment';
 import { underscore } from '@ember/string';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   session: service(),
+  // old and Deprecated method;
+  // TODO(abhishek): Should be remove it?
   authorize (xhr) {
     let { key } = this.get('session.data.authenticated')
     xhr.setRequestHeader('Authorization', `Bearer ${key}`);
   },
+  // below works on newer ember-simple-auth
+  headers: computed('session.data.authenticated', function () {
+    let { key } = this.get('session.data.authenticated')
+    return {
+      'Authorization':  `Bearer ${key}`
+    }
+  }),
   host: env.apiHost,
   namespace: 'api',
   pathForType: function (type) {
